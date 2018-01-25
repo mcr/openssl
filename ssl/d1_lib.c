4a8a451ca72f4fe83f5954fed1efbc045e3b6ff7
Michael Richardson <mcr@sandelman.ca>
2018-01-25 13:11:14 -0500

    copy data from init_buf->data rather than init_buf

--- ssl/d1_lib.c
+++ ssl/d1_lib.c
@@ -944,7 +944,7 @@ int DTLSv1_accept(SSL *serv, SSL *connection, BIO_ADDR *client, int nfd)
       goto end;
     }
 
-    /* At this point, there is a real ClientHello in s->init_buf */
+    /* At this point, there is a real ClientHello in serv->init_buf */
 
     /*
      * We need to move the init_buf over to connection, set up
@@ -957,7 +957,7 @@ int DTLSv1_accept(SSL *serv, SSL *connection, BIO_ADDR *client, int nfd)
       }
     }
 
-    memcpy(rb->buf, serv->init_buf, serv->init_num);
+    memcpy(rb->buf, serv->init_buf->data, serv->init_num);
     rb->offset = 0;
     rb->left   = serv->init_num;
 
